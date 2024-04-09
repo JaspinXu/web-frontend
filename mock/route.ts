2f975edfa0e20636427e4d76921e0e8a49ca6880
JaspinXu <sea.xuo@gmail.com>
2024-04-09 16:49:20 +0800

    111

--- mock/route.ts
+++ mock/route.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: JaspinXu sea.xuo@gmail.com
+ * @Date: 2024-03-17 12:21:41
+ * @LastEditors: JaspinXu sea.xuo@gmail.com
+ * @LastEditTime: 2024-04-09 16:49:04
+ * @FilePath: \web-frontend\mock\route.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 export default {
   '/api/auth_routes': {
     '/form/advanced-form': { authority: ['admin', 'user'] },
